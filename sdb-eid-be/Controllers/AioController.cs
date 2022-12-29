4b08c1dfb51daa1cf7eae5e79c6fe2542ac91e53
iamnga <ngann250195@gmail.com>
2022-12-29 15:27:16 +0700

    Test API

---
+++ sdb-eid-be/Controllers/AioController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using sdb_eid_be.Models;
+
+namespace sdb_eid_be.Controllers
+{
+    
+    [ApiController]
+    public class AioController : ControllerBase
+    {
+        [Route("[controller]/upload-image")]
+        [HttpPost]
+        public async Task<object> UploadImage(AllInOneRequest req)
+        {
+            var reqJSON = JsonConvert.SerializeObject(req);
+
+            var client = new HttpClient();
+            var response = await client.PostAsync("https://cardtest.sacombank.com.vn:9443/digizone/upload-image", new StringContent(reqJSON, Encoding.UTF8, "application/json"));
+            var contents = await response.Content.ReadAsStringAsync();
+            return contents;
+        }
+    }
+}
---
+++ sdb-eid-be/Models/UploadImage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sdb_eid_be.Models
+{
+    public class UploadImage
+    {
+        public string imageBas64 { get; set; }
+        public string imageType { get; set; }
+    }
+
+    public class AllInOneRequest
+    {
+        public string refNumber { get; set; }
+        public string refDateTime { get; set; }
+        public string deviceID { get; set; }
+        public string sessionID { get; set; }
+        public UploadImage data { get; set; }
+    }
+}
